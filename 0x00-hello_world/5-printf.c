48f67706d77e96133e02633bc8e64a301f8587ef
Simeon <bangurasimeonn@gmail.com>
2023-10-23 19:54:57 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - it allows the printing of the c program
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
