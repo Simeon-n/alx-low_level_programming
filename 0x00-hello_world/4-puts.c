18208a4a97f9721fc689be7a8194c1c1a27c3f79
Simeon <bangurasimeonn@gmail.com>
2023-10-23 19:23:03 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - this is the main function
- * 
+ *
  * Return: always 0
  */
 
@@ -10,4 +10,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
-}	
+}
