9252aebc4a2f248008d3c6429d4b34cacfeba067
Simeon <bangurasimeonn@gmai.com>
2023-12-01 09:58:14 -0800

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,22 @@
 
 int main(void)
 {
-	int i;
+	int d, p;
 
-	for (i = 01 ; i < 100 ; i++)
-		putchar(i + 0);
-	if (i != 00)
+	for (d = '0'; d < '9' ; d++)
 	{
-		putchar(i);
-		putchar(',');
-		putchar(' ');
+	for (p = d + 1; p <= '9'; p++)
+	{
+	if (p != d)
+	{
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
