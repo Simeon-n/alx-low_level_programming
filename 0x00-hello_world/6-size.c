36bbcf2b01c654380487a9dd9c58879f2c4e38e7
Simeon <bangurasimeonn@gmail.com>
2023-12-09 15:37:42 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: This program does something
- * @parameter: Describe the parameter
- *
- * Return: Always 0 (Success)
+ * main - this sdesribes the main function of the code
+ * Return: 0
  */
+
 int main(void)
 {
 	char a;
 	int b;
 	long int c;
 	long long int d;
-	float f;
+	float e;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
