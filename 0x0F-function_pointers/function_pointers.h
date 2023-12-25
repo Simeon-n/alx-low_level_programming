f7471b7fbfa606e0ce729e4d8149dac0617d41ef
Simeon <bangurasimeonn@gmail.com>
2023-12-25 08:54:37 -0800

    header file and task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,12 @@
-#ifndef _VARIADIC_H_
-#define _VARIADIC_H_
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
+#include <stddef.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
