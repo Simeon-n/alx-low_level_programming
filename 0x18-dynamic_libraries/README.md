e454e1f608b2e84af5ccb971b2defd3c8aa6a3ab
simeon-n <bangurasimeonn@gmail.com>
2024-08-17 04:26:36 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,3 @@
+Create the dynamic library libdynamic.so containing all the functions
+Create a script that creates a dynamic library called liball.so from all the .c files that are in the current directory
+
---
+++ 0x18-dynamic_libraries/functions/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - Programing
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
